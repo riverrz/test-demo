04df63785824ae494ee1cd3179814ad6a132fff4
shivam123425 <shivam123425@gmail.com>
2019-07-19 01:11:48 +0530

    Add stub on jwt.verify for testing

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,5 +1,6 @@
 const expect = require("chai").expect;
-
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
 const authMiddleware = require("../middleware/is-auth");
 
 describe("Auth middleware", function() {
@@ -18,6 +19,28 @@ describe("Auth middleware", function() {
     const req = {
       get: function() {
         return "Bearer";
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+  it("should yield an userId after verification of token ", function() {
+    const req = {
+      get: function() {
+        return "Bearer sadfgdsxcvdsfd";
+      }
+    };
+    sinon.stub(jwt, "verify");
+    // addded by sinon
+    jwt.verify.returns({ userId: "abc" });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    jwt.verify.restore();
+  });
+  it("should throw an error if token cannot be verified", function() {
+    const req = {
+      get: function() {
+        return "Bearer xyz";
       }
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
