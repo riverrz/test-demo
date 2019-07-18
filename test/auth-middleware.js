97ba9216b3038714639e2061d2988ccf6f265381
shivam123425 <shivam123425@gmail.com>
2019-07-19 00:51:35 +0530

    Add describe block to group tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,11 +2,24 @@ const expect = require("chai").expect;
 
 const authMiddleware = require("../middleware/is-auth");
 
-it("should throw error if no authorization header present", function() {
-  const req = {
-    get: function() {
-      return null;
-    }
-  };
-  expect(authMiddleware.bind(this,req, {}, () => {})).to.throw("Not authenticated.");
+describe("Auth middleware", function() {
+  it("should throw error if no authorization header present", function() {
+    const req = {
+      get: function() {
+        return null;
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if authrization token is one string", function() {
+    const req = {
+      get: function() {
+        return "Bearer";
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
