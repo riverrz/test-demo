f53aec0294c331121411c301f086a4cc353bbb77
shivam123425 <shivam123425@gmail.com>
2019-07-19 21:59:38 +0530

    Add tests for auth-controller - Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
-const { validationResult } = require('express-validator/check');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.');
+    const error = new Error("Validation failed.");
     error.statusCode = 422;
     error.data = errors.array();
     throw error;
@@ -24,7 +24,7 @@ exports.signup = async (req, res, next) => {
       name: name
     });
     const result = await user.save();
-    res.status(201).json({ message: 'User created!', userId: result._id });
+    res.status(201).json({ message: "User created!", userId: result._id });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -40,14 +40,14 @@ exports.login = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: email });
     if (!user) {
-      const error = new Error('A user with this email could not be found.');
+      const error = new Error("A user with this email could not be found.");
       error.statusCode = 401;
       throw error;
     }
     loadedUser = user;
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-      const error = new Error('Wrong password!');
+      const error = new Error("Wrong password!");
       error.statusCode = 401;
       throw error;
     }
@@ -56,15 +56,17 @@ exports.login = async (req, res, next) => {
         email: loadedUser.email,
         userId: loadedUser._id.toString()
       },
-      'somesupersecretsecret',
-      { expiresIn: '1h' }
+      "somesupersecretsecret",
+      { expiresIn: "1h" }
     );
     res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    return;
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
+    return err;
   }
 };
 
@@ -72,7 +74,7 @@ exports.getUserStatus = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId);
     if (!user) {
-      const error = new Error('User not found.');
+      const error = new Error("User not found.");
       error.statusCode = 404;
       throw error;
     }
@@ -90,13 +92,13 @@ exports.updateUserStatus = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId);
     if (!user) {
-      const error = new Error('User not found.');
+      const error = new Error("User not found.");
       error.statusCode = 404;
       throw error;
     }
     user.status = newStatus;
     await user.save();
-    res.status(200).json({ message: 'User updated.' });
+    res.status(200).json({ message: "User updated." });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
---
+++ test/auth-controller.js
@@ -0,0 +1,28 @@
+const expect = require("chai").expect;
+const sinon = require("sinon");
+
+const User = require("../models/user");
+
+const authController = require("../controllers/auth");
+
+describe("Auth Controller - Login", function() {
+  it("should throw an error with code 500 if accessing the database fails", function(done) {
+    sinon.stub(User, "findOne");
+    User.findOne.throws();
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "tester"
+      }
+    };
+    authController
+      .login(req, {}, () => {})
+      .then(result => {
+        expect(result).to.be.an("error");
+        expect(result).to.have.property("statusCode", 500);
+        done();
+      })
+      .catch(done);
+    User.findOne.restore();
+  });
+});
