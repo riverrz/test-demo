8547fd369e1da7f2143130169e010cf97132f9d6
shivam123425 <shivam123425@gmail.com>
2019-07-19 22:36:56 +0530

    Use test db to test user status

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,11 +1,29 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 
 const authController = require("../controllers/auth");
 
-describe("Auth Controller - Login", function() {
+describe("Auth Controller", function() {
+  before(function(done) {
+    mongoose
+      .connect("mongodb://localhost:27017/test-demo-dev")
+      .then(result => {
+        const user = new User({
+          email: "shivam@example.com",
+          password: "qwerty",
+          name: "Shivam",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a"
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
   it("should throw an error with code 500 if accessing the database fails", function(done) {
     sinon.stub(User, "findOne");
     User.findOne.throws();
@@ -24,5 +42,40 @@ describe("Auth Controller - Login", function() {
       })
       .catch(done);
     User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", function(done) {
+    const req = {
+      userId: "5c0f66b979af55031b34728a"
+    };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function(data) {
+        this.userStatus = data.status;
+      }
+    };
+    authController
+      .getUserStatus(req, res, () => {})
+      .then(() => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.userStatus).to.be.equal("I am new!");
+        done();
+      })
+      .catch(done);
+  });
+
+  after(function(done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
   });
 });
